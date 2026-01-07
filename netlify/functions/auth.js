b79e5f54896087b2f13b2680a6e55a780b79efbf
TrackDates <tylertwerdun@hotmail.com>
2026-01-07 08:04:51 -0700

    Delete netlify/functions/auth.js

--- netlify/functions/auth.js
+++
@@ -1,9 +0,0 @@
-const { Handler } = require("@decapcms/netlify-cms-oauth-provider-node");
-
-const handler = new Handler({
-  // You’ll set these in Netlify environment variables
-  // OAUTH_CLIENT_ID and OAUTH_CLIENT_SECRET
-  // (GitHub OAuth app credentials)
-});
-
-exports.handler = handler.handle.bind(handler);
